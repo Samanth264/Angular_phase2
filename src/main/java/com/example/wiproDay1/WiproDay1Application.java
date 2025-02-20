e2b43243dca52a3d7b03ad3dc3adc41973043a22
Samanth264 <samanthkumar535@gmail.com>
2025-02-20 11:20:48 +0530

    Added wiproDay1 Spring Boot project

---
+++ src/main/java/com/example/wiproDay1/WiproDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.wiproDay1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WiproDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WiproDay1Application.class, args);
+	}
+
+}
