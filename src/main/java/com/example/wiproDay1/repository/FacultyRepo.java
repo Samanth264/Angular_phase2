e1053246cbd5a5dca981c2e8864ce538ecc54f18
Samanth264 <samanthkumar535@gmail.com>
2025-02-21 10:09:07 +0530

    Removed all files to re-upload fresh project structure

--- src/main/java/com/example/wiproDay1/repository/FacultyRepo.java
+++
@@ -1,9 +0,0 @@
-package com.phase2.wiproDay1.Repository;
-
-import com.phase2.wiproDay1.Model.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FacultyRepo extends JpaRepository<Faculty, Long> {
-}
